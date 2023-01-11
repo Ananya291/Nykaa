b3248ec9d95cc9905ede0ef3b67691938bcea80f
Ananya291 <ananyat9999@gmail.com>
2023-01-11 17:54:41 +0530

    Adding files

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <div>
+        {/* Logo */}
+        <img></img>
+        {/* searchBar */}
+        <input></input>
+      </div>
+      <div>
+        <h1>Hello,ANANYA</h1>
+        <h1>Return and order</h1>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/NewFile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "./Banner";
+import "./NewFile.css";
+
+function NewFile() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>jkahvpowajkv</p>
+      <Banner />
+    </div>
+  );
+}
+
+export default NewFile;
